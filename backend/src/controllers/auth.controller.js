f2a3f44756bb518b1afe652e27c6e1cfdd06f269
Rutik Yerawar <rutikyerawar2605@gmail.com>
2025-02-27 17:05:29 +0530

    Initial Setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+export const signup = (req, res) => {
+    res.send("signup route");
+}
+
+export const login = (req, res) => {
+    res.send("login route");
+}
+
+export const logout = (req, res) => {
+    res.send("logout route");
+}
