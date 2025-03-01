eac95befcaafef599801e076d9a640fb987cc6c2
Rutik Yerawar <rutikyerawar2605@gmail.com>
2025-03-01 17:37:28 +0530

    Profile Page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -40,7 +40,7 @@ export const useAuthStore = create(( set) => ({
           toast.error(error.response.data.message);
         }
       },
-      login: async (data) => {
+    login: async (data) => {
         set({ isLoggingIn: true });
         try {
           const res = await axiosInstance.post("/auth/login", data);
@@ -54,4 +54,18 @@ export const useAuthStore = create(( set) => ({
           set({ isLoggingIn: false });
         }
       },
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+          const res = await axiosInstance.put("/auth/update-profile", data);
+          set({ authUser: res.data });
+          toast.success("Profile updated successfully");
+        } catch (error) {
+          console.log("error in update profile:", error);
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isUpdatingProfile: false });
+        }
+      },
+    
 }))
